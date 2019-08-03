45e16a3bfd65f6358f79dda5edf369997a35d127
Nithesh Kg <nitheshkg18@gmail.com>
2019-08-03 11:23:03 +0530

    k nearest neighbor scractch 1

--- e15.py
+++ e15.py
@@ -1,7 +1,39 @@
+import numpy as np
 from math import sqrt
+import matplotlib.pyplot as plt
+from matplotlib import style
+from collections import Counter
+import warnings
+style.use('fivethirtyeight')
 
-plot1=[1,3]
-plot2=[2,5]
 
-eucledian_distance=sqrt((plot1[0]-plot2[0])**2 + (plot1[1]-plot2[1])**2 )
-print(eucledian_distance)
+
+dataset = {'k':[[1,2],[2,3],[3,1]],'r':[[6,7],[7,7],[8,6]]}
+new_features=[5,7]
+
+
+
+##for i in dataset:
+##    for ii in dataset[i]:
+##        [plt.scatter(ii[o],ii[1],s=100,color=i)]
+
+        #OR
+
+##[[plt.scatter(ii[0],ii[1],s=100,color=i) for ii in dataset[i]]for i in dataset]
+##plt.scatter(new_features[0],new_features[1])
+##plt.show()
+
+def k_nearest_neighbors(data,predict,k=3):
+    if(len(data)>=3):
+        warnings.warn('You are an idiot the data is greater than k')
+
+    knnalgos
+    return vote_result
+
+
+
+#plot1=[1,3]
+#plot2=[2,5]
+
+#eucledian_distance=sqrt((plot1[0]-plot2[0])**2 + (plot1[1]-plot2[1])**2 )
+#print(eucledian_distance)
